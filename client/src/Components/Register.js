8022b918446b51a4583d2ad9f577f9eb858ee7f6
Rafiaa46 <46S186@utas.edu.om>
2025-03-04 09:17:00 +0400

    added userSlice

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,4 +1,9 @@
 import loginImage from "../Images/loginImage.jpg";
+import { userSchemaValidation } from "../Validations/UserValidations";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import {
   Button,
   Col,
@@ -10,29 +15,20 @@ import {
   Form,
 } from "reactstrap";
 import logo from "../Images/logo-t.png";
-import { userSchemaValidation } from "../Validations/UserValidations";
-import * as yup from "yup";
-
-
-
-import { useForm } from "react-hook-form";
-
-import { yupResolver } from "@hookform/resolvers/yup";
-
 
 const Register = () => {
-const {register ,
-   handleSubmit, 
-   formState:{errors}}=useForm({
-  resolver:yupResolver(userSchemaValidation)
-});
-// Handle form submission
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(userSchemaValidation) });
 
-const onSubmit = (data) => {
+  // Handle form submission
 
-  console.log("Form Data", data); // You can handle the form submission here
+  const onSubmit = (data) => {
+    console.log("Form Data", data); // You can handle the form submission here
+  };
 
-}
   return (
     <Container>
       <h1>Register</h1>
@@ -40,30 +36,38 @@ const onSubmit = (data) => {
         <Row>
           <Col md={6}>
             Name<br></br>
-            <input type="text" name="name"  {...register("name")}></input>
-            <p className="error">{errors.name?.message}</p>
+            <input type="text" name="name" {...register("name")}></input>
           </Col>
+          <p className="error">{errors.name?.message}</p>
         </Row>
         <Row>
           <Col md={6}>
             Email<br></br>
-            <input type="email" name="email"  {...register("email")}></input>
-            <p className="error">{errors.email?.message}</p>
+            <input type="email" name="email" {...register("email")}></input>
           </Col>
+          <p className="error">{errors.email?.message}</p>
         </Row>
         <Row>
           <Col md={6}>
             Password<br></br>
-            <input type="password" name="password" {...register("password")}></input>
-            <p className="error">{errors.password?.message}</p>
+            <input
+              type="password"
+              name="password"
+              {...register("password")}
+            ></input>
           </Col>
+          <p className="error">{errors.password?.message}</p>
         </Row>
         <Row>
           <Col md={6}>
             Confirm Password<br></br>
-            <input type="password" name="confirmpassword" {...register("confirmPassword")}></input>
-            <p className="error">{errors.confirmPassword?.message}</p>
+            <input
+              type="password"
+              name="confirmpassword"
+              {...register("confirmPassword")}
+            ></input>
           </Col>
+          <p className="error">{errors.confirmPassword?.message}</p>
         </Row>
         <Row>
           <Col md={6}>
--- client/src/Validation/UserValidation.js
+++
@@ -1,5 +0,0 @@
-import * as yup from "yup";
-
-export const userSchemaValidation=yup.object().shape({
-    
-});
